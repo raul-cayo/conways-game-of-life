bf4ca4a7ca9e70184868126de701ac3e4f709c83
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2021-09-04 11:45:25 -0500

    Change App to class and use timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Building Conway's Game Of Life</p>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: new Date()
+    };
+  }
+
+  // Lyfecycle Methods //
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  // Component Methods //
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Building Conway's Game Of Life</p>
+          <p>{this.state.date.toLocaleTimeString()}</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
