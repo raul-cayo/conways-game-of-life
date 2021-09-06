68e59848a4a4ac79b7f3e6d42eb1c1721227361b
Raul Sanchez Cayo <raul.sanchez.cayo@gmail.com>
2021-09-05 21:59:25 -0500

    Create game of life logic and grid

--- src/App.js
+++ src/App.js
@@ -8,7 +8,19 @@ class App extends React.Component {
     this.state = {
       counter: 0,
       speed: 1000,
-      generation: [0, 1, 0, 1, 1, 0],
+      xSize: 9,
+      ySize: 9,
+      generation: [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ],
       onColor: 'indianred',
       offColor: 'black',
       cellSize: '70px',
@@ -33,7 +45,47 @@ class App extends React.Component {
   nextGeneration() {
     const timerID = setTimeout(this.nextGeneration, this.state.speed);
     this.setState((prevState) => {
-      const newGeneration = prevState.generation.map(cellState => !cellState);
+      const newGeneration = [];
+      for (let y = 0; y < this.state.ySize; y++) {
+        newGeneration.push([]);
+        for(let x = 0; x < this.state.xSize; x++) {
+          let neighbors = 0;
+          if (y > 0) {
+            neighbors += prevState.generation[y - 1][x];
+            if (x < this.state.xSize - 1) {
+              neighbors += prevState.generation[y - 1][x + 1];
+            }
+            if (x > 0) {
+              neighbors += prevState.generation[y - 1][x - 1];
+            }
+          }
+          if (y < this.state.ySize - 1) {
+            neighbors += prevState.generation[y + 1][x];
+            if (x < this.state.xSize - 1) {
+              neighbors += prevState.generation[y + 1][x + 1];
+            }
+            if (x > 0) {
+              neighbors += prevState.generation[y + 1][x - 1];
+            }
+          }
+          if (x > 0) {
+            neighbors += prevState.generation[y][x - 1];
+          }
+          if (x < this.state.xSize - 1) {
+            neighbors += prevState.generation[y][x + 1];
+          }
+
+          // Creating new generation
+          if (neighbors === 3) {
+            newGeneration[y][x] = 1;
+          } else if (prevState.generation[y][x] && (neighbors === 2 || neighbors === 3)) {
+            newGeneration[y][x] = 1;
+          } else {
+            newGeneration[y][x] = 0;
+          }
+        }
+      }
+
       return {
         counter: prevState.counter + 1,
         speed: prevState.speed,
@@ -58,10 +110,16 @@ class App extends React.Component {
             cellSize={this.state.cellSize}/>
         </div>
         <div className="controls-container">
-          <input type="number"
-            value={this.state.speed}
-            onChange={this.handleSpeedChange}/>
-          <p>{this.state.counter}</p>
+          <div className="control">
+            <p>Speed</p>
+            <input type="number"
+              value={this.state.speed}
+              onChange={this.handleSpeedChange}/>
+          </div>
+          <div className="control">
+            <p>Counter</p>
+            <p>{this.state.counter}</p>
+          </div>
         </div>
       </div>
     );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,13 +3,18 @@ import Cell from './Cell';
 
 class Grid extends React.Component {
   render() {
-    return this.props.generation.map((cellState) => 
-      <Cell
-        isOn={cellState}
-        onColor={this.props.onColor}
-        offColor={this.props.offColor}
-        size={this.props.cellSize}/>
-    );
+    const rows = this.props.generation.map((row) => {
+      const cells = row.map((cellState) =>
+        <Cell
+          isOn={cellState}
+          onColor={this.props.onColor}
+          offColor={this.props.offColor}
+          size={this.props.cellSize}/>
+      );
+      return <div className="grid-row">{cells}</div>;
+    });
+
+    return <div className="grid">{rows}</div>;
   }
 }
 
